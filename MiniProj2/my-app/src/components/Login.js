f82819352039bf00c755420280dd40ac171df979
sam-moore98 <sam98.moore@hotmail.com>
2023-02-20 22:03:02 +1100

    commit

---
+++ MiniProj2/my-app/src/components/Login.js
@@ -0,0 +1,33 @@
+import React, {useRef} from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+export default function LoginPage() {
+
+  const username = useRef()
+  const password = useRef()
+  const handleSubmit=()=>{
+      if(username.current.value=='sammoore'&&password.current.value=='1234'){
+        localStorage.setItem("usernameData", "sammoore")
+        localStorage.setItem("passwordData", "1234")
+      }
+  }
+
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input type="text" placeholder="Username / E-mail" id="username" ref={username} label="Username / E-mail" variant="filled" />
+        </div>
+        <div>
+          <input type="password" placeholder="Password" id="password" ref={password} label="Password" variant="filled" />
+        </div><br/>
+        <button variant="contained"> Login! </button>
+        
+      </form>
+    </div>
+    
+  );
+}
--- MiniProj2/my-app/src/pages/Login.js
+++ MiniProj2/my-app/src/pages/Login.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import LoginPage from '../components/Login';
 
+function Login(){
+    return(
+        <div>
+            <h1> Login Here:</h1>
+            
+            <LoginPage />
+        </div>
+        )
+    }
+    
+    export default Login;
